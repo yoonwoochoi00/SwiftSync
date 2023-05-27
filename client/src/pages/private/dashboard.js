87ce249d4cbe135a55a92d9aec94edafcb8668a8
yunuchoi621 <91050411+yunuchoi621@users.noreply.github.com>
2023-05-27 12:54:08 +1200

    Improve dashboard UI

--- client/src/pages/private/dashboard.js
+++ client/src/pages/private/dashboard.js
@@ -3,6 +3,19 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 
+// Component imports
+import { mainListItems, secondaryListItems } from '../../components/private/listItems';
+import Chart from '../../components/private/chart';
+import Sales from '../../components/private/sales';
+import Orders from '../../components/private/orders';
+import AppBar from "../../components/private/appBar";
+import Drawer from "../../components/private/drawer";
+import Copyright from '../../components/public/copyright';
+
+// Design related import
+import logoBlack from "../../logo_black.png"
+
+// Material UI imports
 import Toolbar from '@mui/material/Toolbar';
 import List from '@mui/material/List';
 import Typography from '@mui/material/Typography';
@@ -16,19 +29,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 
-// Component imports
-import { mainListItems, secondaryListItems } from '../../components/private/listItems';
-import Chart from '../../components/private/chart';
-import Sales from '../../components/private/sales';
-import Orders from '../../components/private/orders';
-import AppBar from "../../components/private/appBar";
-import Drawer from "../../components/private/drawer";
-import Copyright from '../../components/public/copyright';
-
-// Design related import
-import logoBlack from "../../logo_black.png"
-
-// TODO remove, this demo shouldn't need to reset the theme.
 const dashboardTheme = createTheme({
     palette: {
         primary: {
@@ -58,115 +58,117 @@ export default function Dashboard() {
 
     return (
         <ThemeProvider theme={dashboardTheme}>
-        <Box sx={{ display: 'flex' }}>
-            <CssBaseline />
-            <AppBar position="absolute" open={open}>
-            <Toolbar
-                sx={{
-                pr: '24px', // keep right padding when drawer closed
-                }}
-            >
-                <IconButton
-                edge="start"
-                color="inherit"
-                aria-label="open drawer"
-                onClick={toggleDrawer}
-                sx={{
-                    marginRight: '36px',
-                    ...(open && { display: 'none' }),
-                }}
-                >
-                <MenuIcon />
-                </IconButton>
-                <Typography
-                component="h1"
-                variant="h6"
-                color="inherit"
-                noWrap
-                sx={{ flexGrow: 1 }}
-                >
-                Dashboard
-                </Typography>
-                <IconButton color="inherit">
-                <Badge badgeContent={4} color="secondary">
-                    <NotificationsIcon />
-                </Badge>
-                </IconButton>
-            </Toolbar>
-            </AppBar>
-            <Drawer variant="permanent" open={open}>
-                <Toolbar
-                    sx={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'flex-end',
-                        px: [1],
-                    }}
-                >
-                    <img src={logoBlack} style={{ width: "160px" }} alt="SwiftSync Logo"/>
-                    <IconButton onClick={toggleDrawer}>
-                    <ChevronLeftIcon />
-                    </IconButton>
-                </Toolbar>
-                <Divider />
-                <List component="nav">
-                    {mainListItems}
-                    <Divider sx={{ my: 1 }} />
-                    {secondaryListItems}
-                </List>
-            </Drawer>
-            <Box
-            component="main"
-            sx={{
-                backgroundColor: (theme) =>
-                theme.palette.mode === 'light'
-                    ? theme.palette.grey[100]
-                    : theme.palette.grey[900],
-                flexGrow: 1,
-                height: '100vh',
-                overflow: 'auto',
-            }}
-            >
-            <Toolbar />
-            <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-                <Grid container spacing={3}>
-                {/* Chart */}
-                <Grid item xs={12} md={8} lg={9}>
-                    <Paper
-                    sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 240,
-                    }}
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+                <AppBar position="absolute" open={open}>
+                    <Toolbar
+                        sx={{
+                        pr: '24px', // Keep right padding when drawer closed
+                        }}
                     >
-                    <Chart />
-                    </Paper>
-                </Grid>
-                {/* Recent sales */}
-                <Grid item xs={12} md={4} lg={3}>
-                    <Paper
-                    sx={{
-                        p: 2,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        height: 240,
-                    }}
+                        <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={toggleDrawer}
+                        sx={{
+                            marginRight: '36px',
+                            ...(open && { display: 'none' }),
+                        }}
+                        >
+                        <MenuIcon />
+                        </IconButton>
+                        <Typography
+                        component="h1"
+                        variant="h6"
+                        color="inherit"
+                        noWrap
+                        sx={{ flexGrow: 1 }}
+                        >
+                        Dashboard
+                        </Typography>
+                        <IconButton color="inherit">
+                        <Badge badgeContent={4} color="secondary">
+                            <NotificationsIcon />
+                        </Badge>
+                        </IconButton>
+                    </Toolbar>
+                </AppBar>
+                <Drawer variant="permanent" open={open}>
+                    <Toolbar
+                        sx={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'flex-end',
+                            px: [1],
+                        }}
                     >
-                    <Sales />
-                    </Paper>
-                </Grid>
-                {/* Recent orders */}
-                <Grid item xs={12}>
-                    <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
-                    <Orders />
-                    </Paper>
-                </Grid>
-                </Grid>
-                <Copyright sx={{ pt: 4 }} />
-            </Container>
+                        <img src={logoBlack} style={{ width: "160px" }} alt="SwiftSync Logo"/>
+                        <IconButton onClick={toggleDrawer}>
+                        <ChevronLeftIcon />
+                        </IconButton>
+                    </Toolbar>
+                    <Divider />
+                    <List component="nav">
+                        {mainListItems}
+                        <Divider sx={{ my: 1 }} />
+                        {secondaryListItems}
+                    </List>
+                </Drawer>
+                <Box
+                component="main"
+                sx={{
+                    backgroundColor: (theme) =>
+                    theme.palette.mode === 'light'
+                        ? theme.palette.grey[100]
+                        : theme.palette.grey[900],
+                    flexGrow: 1,
+                    height: '100vh',
+                    overflow: 'auto',
+                }}
+                >
+                <Toolbar />
+                <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+                    <Grid container spacing={3}>
+                        {/* Chart */}
+                        <Grid item xs={12} md={8} lg={9}>
+                            <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240,
+                            }}
+                            >
+                                <Chart />
+                            </Paper>
+                        </Grid>
+
+                        {/* Recent sales */}
+                        <Grid item xs={12} md={4} lg={3}>
+                            <Paper
+                            sx={{
+                                p: 2,
+                                display: 'flex',
+                                flexDirection: 'column',
+                                height: 240,
+                            }}
+                            >
+                                <Sales />
+                            </Paper>
+                        </Grid>
+
+                        {/* Recent orders */}
+                        <Grid item xs={12}>
+                            <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                            <Orders />
+                            </Paper>
+                        </Grid>
+                    </Grid>
+                    <Copyright sx={{ pt: 4 }} />
+                </Container>
+                </Box>
             </Box>
-        </Box>
         </ThemeProvider>
     );
 }
